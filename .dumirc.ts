abc85fdc0124e03d5c8f00a74e95cdfa0013075d
weijunze <xaweijunze@163.com>
2024-10-15 17:09:40 +0800

    feat(docs): update logo footer

--- .dumirc.ts
+++ .dumirc.ts
@@ -3,6 +3,8 @@ import { defineConfig } from 'dumi';
 export default defineConfig({
   outputPath: 'docs-dist',
   themeConfig: {
-    name: 'ww-zzhooks',
+    name: 'zzhooks',
+    logo: '/logo.png',
+    footer: `冀ICP备2021014913号 · 冀公网安备13310302000073号`,
   },
 });
